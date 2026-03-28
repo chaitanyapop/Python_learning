38a72c12efdd1d5ba8fa2397990deb298d912121
chaitanyapop <parandkarchaitanya@gmail.com>
2026-03-28 08:51:54 +0530

    minor change

--- string_assignment2.py
+++ string_assignment2.py
@@ -1,4 +1,4 @@
 userName = input("Enter your name: ")
 daysRemaining = input("Days remaining for your birthday: ")
 dayToWeekConvert = round(int(daysRemaining)/7, 2)
-print(f"Username is {userName} and weeks left for user's birthday are {dayToWeekConvert}")
+print(f"Username is {userName} and weeks left for user's birthday are {dayToWeekConvert} yes")
